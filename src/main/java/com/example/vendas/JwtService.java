51c284806abebe12c5194fa8beb66b8022c515cc
lunatic0696 <lunatictan96@gmail.com>
2022-10-28 13:18:07 -0300

    Início do JWT

--- src/main/java/com/example/vendas/JwtService.java
+++ src/main/java/com/example/vendas/JwtService.java
@@ -1,15 +1,20 @@
 package com.example.vendas;
 
 import com.example.vendas.domain.Entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
 
 @Service
 public class JwtService {
@@ -19,15 +24,55 @@ public class JwtService {
     @Value("${security.jwt.chave-assinatura")
     private String chaveAssinatura;
 
-    public String gerarToken(Usuario usuario){
+    public String gerarToken(Usuario usuario) {
         long expString = Long.valueOf(expiracao);
         LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
         Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
         Date data = Date.from(instant);
+        HashMap<String, Object> claims = new HashMap<>(); //Claims são para colocar informações adicionais, como Roles
+        claims.put("email", "exemplo@exemplo.com");
         return Jwts.builder()
                 .setSubject(usuario.getLogin())
                 .setExpiration(data)
+                .setClaims(claims)
                 .signWith(SignatureAlgorithm.HS512, chaveAssinatura).compact();
+    }
+
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido(String token) {
+        try {
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data =
+                    dataExpiracao.toInstant()
+                            .atZone(ZoneId.systemDefault()).toLocalDateTime(); //Converter Date para LocalDateTime
+            return !LocalDateTime.now().isAfter(data);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException{
+        return (String) obterClaims(token).getSubject();
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext contexto = SpringApplication.run(VendasApplication.class);
+        JwtService service = contexto.getBean(JwtService.class);
+        Usuario usuario = Usuario.builder().login("fulano").build();
+        String token = service.gerarToken(usuario);
+        System.out.println(token);
+
+        boolean isTokenValido = service.tokenValido(token);
+        System.out.println("O Token está válido? " + isTokenValido);
+        System.out.println(service.obterLoginUsuario(token));
     }
 
 }
